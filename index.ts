14c555c32351467e2866e5eeefcadd7b59b6f392
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-05-12 10:21:27 -0300

    12/5

--- index.ts
+++ index.ts
@@ -2,8 +2,5 @@ import DbService from "./src/services/db-service.ts"
 
 console.clear()
 
-const dbService = new DbService()
+const dbService: DbService = new DbService()
 console.log("getEdificios", await dbService.getEdificios())
-console.log("getIncidentes", await dbService.getIncidentes())
-console.log("getAulaById", await dbService.getAulaById(1))
-console.log("getPisoById", await dbService.getPisoById(2))
