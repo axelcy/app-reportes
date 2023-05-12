ddf3fa49cbd8b6a8eb83fc354eb99120571182aa
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-05-12 10:39:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  cosnt [data, setData] = useState([])
+  const useFetch = async(endpoint) => {
+    try {
+      const response = fetch(`https://localhost${endpoint}`)
+      return await response.json()
+    }
+    catch (err) {
+      console.log(err)
+      throw new Error("No se pudo realizar el fetch a la api")
+    }
+  }
+  useEffect(async() => setData(await useFetch("/Edificios"), []))
   return (
     <>
-    <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+      <p>{data ? JSON.stringify(data) : "..."}</p>
     </>
   )
 }
