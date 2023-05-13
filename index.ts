060a8738dbd0e8ee9c973aa924c8dc1f0a4d3e74
DarkApple55 <axelcy@gmail.com>
2023-05-13 16:16:34 -0300

    funciona todo!

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import DbService from "./src/services/db-service.ts"
 import express from 'express'
 import cors from 'cors'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-console.clear()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 const app = express()
 const port = 3001
 const dbService: DbService = new DbService()
@@ -15,6 +18,8 @@ const routes = Object.freeze({
     usuarios: '/usuarios',
 })
 
+console.clear()
+
 // Set up server
 app.use(cors())
 app.listen(port, () => console.log(` * Example app listening on port ${port}`))
@@ -24,4 +29,8 @@ app.get('/', (_req: any, _res: any) => _res.send(`Reportes api!`))
 app.get(routes.edificios, async(_req: any, _res: any) => _res.send(await dbService.getEdificios()))
 
 // Images
-app.get(`${routes.img}:img`, async(_req: any, _res: any) => _res.sendFile(`${__dirname}/src/img/${_req.params.img}`))
+app.get(`${routes.img}/:img`, async(_req: any, _res: any) => {
+    _res.sendFile(`${__dirname}/src/img/${_req.params.img}`, (err: any) => {
+        err && _res.sendFile(`${__dirname}/src/img/error.jpg`)
+    })
+})
