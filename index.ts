4b232069fd625e3c7790ddfa3e6da5da51b90df6
DarkApple55 <axelcy@gmail.com>
2023-05-13 13:54:43 -0300

    img

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import DbService from "./src/services/db-service.ts"
 import express from 'express'
+import cors from 'cors'
 
 console.clear()
 
@@ -7,13 +8,20 @@ const app = express()
 const port = 3001
 const dbService: DbService = new DbService()
 const routes = Object.freeze({
+    img: '/img',
     edificios: '/edificios',
     pisos: '/pisos',
     aulas: '/aulas',
     usuarios: '/usuarios',
 })
 
+// Set up server
+app.use(cors())
 app.listen(port, () => console.log(` * Example app listening on port ${port}`))
-app.get('/', (_req: any, res: any) => res.send(`Reportes api!`))
+app.get('/', (_req: any, _res: any) => _res.send(`Reportes api!`))
 
-app.get(routes.edificios, async(_req: any, res: any) => res.send(await dbService.getEdificios()))
+// Routes
+app.get(routes.edificios, async(_req: any, _res: any) => _res.send(await dbService.getEdificios()))
+
+// Images
+app.get(`${routes.img}:img`, async(_req: any, _res: any) => _res.sendFile(`${__dirname}/src/img/${_req.params.img}`))
