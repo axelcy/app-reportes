2e306528d1f10d7b292b892e6476d6c3b3c1a1d3
DarkApple55 <axelcy@gmail.com>
2023-05-21 12:19:48 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,17 +12,24 @@ const port = 3001
 const dbService: DbService = new DbService()
 const routes = Object.freeze({
     img: '/img',
+
     getEdificios: '/edificios',
-    getPisos: '/pisos',
-    getAulas: '/aulas',
-    getUsuarios: '/usuarios',
-    getIncidentes: '/incidentes',
-    
     getEdificioById: '/edificio',
+
+    getPisos: '/pisos',
     getPisoById: '/piso',
+    getPisosByEdificio: '/piso/edificio',
+
+    getAulas: '/aulas',
     getAulaById: '/aula',
+    getAulasByPiso: '/aula/piso',
+
+    getUsuarios: '/usuarios',
     getUsuarioById: '/usuario',
+
+    getIncidentes: '/incidentes',
     getIncidenteById: '/incidente'
+    
 })
 
 console.clear()
@@ -33,29 +40,40 @@ app.listen(port, () => console.log(` * Example app listening on port ${port}`))
 app.get('/', (_req: any, _res: any) => _res.send(`Reportes api!`))
 
 // Routes
+
+// EDIFICIOS
+
 app.get(routes.getEdificios, async(_req: any, _res: any) => _res.send(await dbService.getEdificios()))
-app.get(`${routes.getEdificios}/:id`, async(_req: any, _res: any) => {
+app.get(`${routes.getEdificioById}/:id`, async(_req: any, _res: any) => {
     _res.send(await dbService.getEdificioById(Number(_req.params.id)))
 })
 
+// PISOS
+
 app.get(routes.getPisos, async(_req: any, _res: any) => _res.send(await dbService.getPisos()))
-app.get(`${routes.getPisos}/:id`, async(_req: any, _res: any) => {
+app.get(`${routes.getPisoById}/:id`, async(_req: any, _res: any) => {
     _res.send(await dbService.getPisoById(Number(_req.params.id)))
 })
-app.get(`${routes.getPisos}/edificio/:id`, async(_req: any, _res: any) => {
+app.get(`${routes.getPisosByEdificio}/:id`, async(_req: any, _res: any) => {
     _res.send(await dbService.getPisosByEdificio(Number(_req.params.id)))
 })
 
+// AULAS
+
 app.get(routes.getAulas, async(_req: any, _res: any) => _res.send(await dbService.getAulas()))
-app.get(`${routes.getAulas}/:id`, async(_req: any, _res: any) => {
+app.get(`${routes.getAulaById}/:id`, async(_req: any, _res: any) => {
     _res.send(await dbService.getAulaById(Number(_req.params.id)))
 })
-app.get(`${routes.getAulas}/piso/:id`, async(_req: any, _res: any) => {
+app.get(`${routes.getAulasByPiso}/:id`, async(_req: any, _res: any) => {
     _res.send(await dbService.getAulasByPiso(Number(_req.params.id)))
 })
 
+// USUARIOS
 app.get(routes.getUsuarios, async(_req: any, _res: any) => _res.send(await dbService.getUsuarios()))
+
+// INCIDENTES
 app.get(routes.getIncidentes, async(_req: any, _res: any) => _res.send(await dbService.getIncidentes('importancia')))
+
 
 // Images
 app.get(`${routes.img}/:img`, async(_req: any, _res: any) => {
