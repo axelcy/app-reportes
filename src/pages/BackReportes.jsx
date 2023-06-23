8cf93d0d1c37356a652a6b7efa2dbe5f6d324208
Santiago Kasses <101513911+santiagokasses@users.noreply.github.com>
2023-06-23 12:02:07 -0300

    var reportes casi trminado

--- src/pages/BackReportes.jsx
+++ src/pages/BackReportes.jsx
@@ -2,36 +2,50 @@ import { useEffect, useState } from "react"
 import NavBar from "../Components/NavBar"
 import useFetch from "../Hooks/useFetch"
 import Reporte from "../Components/Reporte"
-import './MisReportes.css'
+import './BackReportes.css'
 import { Container } from "react-bootstrap"
-
+import Dropdown from 'react-bootstrap/Dropdown'
+import { FaFilter } from 'react-icons/fa'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 const BackReportes = () => {
-    const [reportes, setReportes] = useState([])
-    // no se cambian los reportes cuando se cambia de usuario, hace falta recargar la pag
+    const [listaReportes, setListaReportes] = useState([])
+    const [reportesActivos, setReportesActivos] = useState([])
+
     useEffect(() => async () => {
-        try {
-            setReportes(await useFetch('/incidentes'))
-        } catch {
-            setReportes([])
-        }
+        const reportes = await useFetch('/incidentes/estado/1') // reportes a resolver
+        setListaReportes(reportes)
+        setReportesActivos(reportes)
     }, [])
+
+    const handleFilters = async (e) => {
+        if (e.target.name === 'todo') setReportesActivos(listaReportes)
+        if (e.target.name === 'importancia') setReportesActivos(listaReportes.filter(({importancia}) => importancia === 3))
+        if (e.target.name === 'categoria') setReportesActivos(listaReportes.filter(({categoria}) => categoria === 1))
+    }
     return (
         <>
             <NavBar />
             <Container>
-            <div className="mis-reportes-container">
-                <h1>Mis Reportes</h1>
-                <div className="lista-reportes-container">
-                    {
-                        !reportes.length ? <h3>Acá aparecerán tus reportes</h3> :
-                        reportes.map((reporte, index) => (
-                            
-                            <Reporte key={index} reporte={reporte} />
-                        ))
-                    }
+                <div className="mis-reportes-container">
+                    <h1>Reportes</h1>
+                    <Dropdown>
+                            <Dropdown.Toggle variant="info" className="toggle" id="dropdown-basic"><FaFilter /> Filtros</Dropdown.Toggle>
+                            <Dropdown.Menu className="dropdown">
+                                <Dropdown.Item name="todo" onClick={handleFilters}>Todos</Dropdown.Item>
+                                <Dropdown.Item name="importancia" onClick={handleFilters}>Importancia alta</Dropdown.Item>
+                                <Dropdown.Item name="categoria" onClick={handleFilters}>informatica</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    <div className="lista-reportes-container">
+                        {
+                            !reportesActivos.length ? <h3>Acá aparecerán tus reportes</h3> :
+                            reportesActivos.map((reporte, index) => (
+                                    <Reporte key={index} reporte={reporte} />
+                                ))
+                        }
+                    </div>
                 </div>
-            </div>
             </Container>
         </>
     )
