a8d7b592e92dd599e52fd8b1c90392b5682a833f
DarkApple55 <axelcy@gmail.com>
2023-06-25 00:16:29 -0300

    login required

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -93,15 +93,13 @@ const FormReportes = () => {
     }
     const handleShowImage = mostrar => setShowImage(mostrar)
 
-    if (!usuario && import.meta.env.VITE_AUTH_NEEDED === 'true') {
-        return (
-            <>
-                <NavBar />
-                <h1>Acceso denegado</h1>
-                <h4>Es necesario estar logeado para poder reportar</h4>
-            </>
-        )
-    }
+    if (!usuario) return (
+        <>
+            <NavBar />
+            <h1>Acceso denegado</h1>
+            <h4>Es necesario estar logeado para poder reportar</h4>
+        </>
+    )
     return (
         <>
             <NavBar />
