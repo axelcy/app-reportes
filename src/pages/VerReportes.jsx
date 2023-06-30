c2cc407d284b2e55860d41dac61e13cbdf95fef0
Santiago Kasses <101513911+santiagokasses@users.noreply.github.com>
2023-06-30 11:58:52 -0300

    orden asc / desc

--- src/pages/VerReportes.jsx
+++ src/pages/VerReportes.jsx
@@ -3,13 +3,14 @@ import NavBar from "../Components/NavBar"
 import useFetch from "../Hooks/useFetch"
 import Reporte from "../Components/Reporte"
 import './VerReportes.css'
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import Dropdown from 'react-bootstrap/Dropdown'
 import { FaFilter } from 'react-icons/fa'
 
 const VerReportes = () => {
     const [listaReportes, setListaReportes] = useState([])
     const [reportesActivos, setReportesActivos] = useState([])
+    const [ascOrder, setAscOrder] = useState(true)
 
     useEffect(() => async () => {
         const reportes = await useFetch('/incidentes/estado/1') // reportes a resolver
@@ -17,10 +18,14 @@ const VerReportes = () => {
         setReportesActivos(reportes)
     }, [])
 
+    const handleOrder = () => {
+        setReportesActivos([...reportesActivos].reverse())
+        setAscOrder(!ascOrder)
+    }
     const handleFilters = async (e) => {
-        if (e.target.name === 'todo') setReportesActivos(listaReportes)
+        if (e.target.name === 'fecha') setReportesActivos([...listaReportes].sort((a, b) => a.fecha - b.fecha))
+        else if (e.target.name === 'edificio') setReportesActivos([...listaReportes].sort((a, b) => a.nombre - b.nombre))
         else if (e.target.name === 'importancia') setReportesActivos([...listaReportes].sort((a, b) => a.importancia - b.importancia))
-        else if (e.target.name === 'categoria') setReportesActivos(listaReportes.filter(({categoria}) => categoria === 1))
     }
 
     return (
@@ -29,22 +34,28 @@ const VerReportes = () => {
             <Container>
                 <div className="mis-reportes-container">
                     <h1>Reportes</h1>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="info" className="toggle" id="dropdown-basic"><FaFilter /> Filtros</Dropdown.Toggle>
-                        <Dropdown.Menu className="dropdown">
-                            <Dropdown.Item name="todo" onClick={handleFilters}>Todos</Dropdown.Item>
-                            <Dropdown.Item name="importancia" onClick={handleFilters}>Importancia</Dropdown.Item>
-                            <Dropdown.Item name="categoria" onClick={handleFilters}>informatica</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <header className="">
+                        <Dropdown>
+                            <div className="divOrder">
+                            <Dropdown.Toggle variant="info" className="toggle" id="dropdown-basic"><FaFilter /> Ordenar por</Dropdown.Toggle>
+                            <Button variant="info" className="toggle" onClick={handleOrder}>Orden: {ascOrder ? 'Asc' : 'Desc'}</Button>
+                            </div>
+                            <Dropdown.Menu className="dropdown">
+                                <Dropdown.Item name="fecha" onClick={handleFilters}>Fecha</Dropdown.Item>
+                                <Dropdown.Item name="edificio" onClick={handleFilters}>Edificio</Dropdown.Item>
+                                <Dropdown.Item name="importancia" onClick={handleFilters}>Importancia</Dropdown.Item>
+                                <Dropdown.Item name="categoria" onClick={handleFilters}>informatica</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </header>
                     <label>Buscar</label>
-                    <input type='text' className="styled-input"/>
+                    <input type='text' className="styled-input" />
                     <div className="lista-reportes-container">
                         {
                             !reportesActivos.length ? <h3>Acá aparecerán tus reportes</h3> :
-                            reportesActivos.map((reporte, index) => (
-                                <Reporte key={index} reporte={reporte} />
-                            ))
+                                reportesActivos.map((reporte, index) => (
+                                    <Reporte key={index} reporte={reporte} />
+                                ))
                         }
                     </div>
                 </div>
