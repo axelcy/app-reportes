039f8bdbba6869f49d305f8309bb77d224bd08db
Santiago Kasses <101513911+santiagokasses@users.noreply.github.com>
2023-08-04 11:52:15 -0300

    Update MisReportes.jsx

--- src/pages/MisReportes.jsx
+++ src/pages/MisReportes.jsx
@@ -5,8 +5,7 @@ import useFetch from "../Hooks/useFetch"
 import Reporte from "../Components/Reporte"
 import './MisReportes.css'
 import { Container, Button } from "react-bootstrap"
-import Dropdown from 'react-bootstrap/Dropdown'
-import { FaFilter } from 'react-icons/fa'
+import OrderxFilter from "../Components/OrderxFilter"
 
 const MisReportes = () => {
     const { usuario } = useUsuario()
@@ -24,26 +23,6 @@ const MisReportes = () => {
         }
     }, [usuario])
 
-    const handleOrder = () => {
-        setReportesActivos([...reportesActivos].reverse())
-        setAscOrder(!ascOrder)
-    }
-    const handleFilters = async (e) => {
-
-        if (ascOrder) {
-            if (e.target.name === "fecha") setReportesActivos([...reportes].sort((a, b) => a.fecha - b.fecha))
-            else if (e.target.name === "edificio") setReportesActivos([...reportes].sort((a, b) => a.idPisoAula - b.idPisoAula))
-            else if (e.target.name === "importancia") setReportesActivos([...reportes].sort((a, b) => a.importancia - b.importancia))
-            else if (e.target.name === "categoria") setReportesActivos([...reportes].sort((a, b) => a.idCategoria - b.idCategoria))
-        }
-        else {
-            if (e.target.name === "fecha") setReportesActivos([...reportes].sort((a, b) => b.fecha - a.fecha))
-            else if (e.target.name === "edificio") setReportesActivos([...reportes].sort((a, b) => b.idPisoAula - a.idPisoAula))
-            else if (e.target.name === "importancia") setReportesActivos([...reportes].sort((a, b) => b.importancia - a.importancia))
-            else if (e.target.name === "categoria") setReportesActivos([...reportes].sort((a, b) => b.idCategoria - a.idCategoria))
-        }
-
-    }
 
     if (!usuario && !import.meta.env.VITE_BYPASS) return (
         <>
@@ -58,18 +37,7 @@ const MisReportes = () => {
             <Container>
                 <div className="mis-reportes-container">
                     <h1>Mis Reportes</h1>
-                <Dropdown>
-                    <div className="divOrder">
-                        <Dropdown.Toggle variant="info" className="toggle" id="dropdown-basic"><FaFilter /> Ordenar por</Dropdown.Toggle>
-                        <Button variant="info" className="toggle" onClick={handleOrder}>Orden: {ascOrder ? 'Asc' : 'Desc'}</Button>
-                    </div>
-                    <Dropdown.Menu className="dropdown">
-                        <Dropdown.Item name="fecha" onClick={handleFilters}>Fecha</Dropdown.Item>
-                        <Dropdown.Item name="edificio" onClick={handleFilters}>Edificio</Dropdown.Item>
-                        <Dropdown.Item name="importancia" onClick={handleFilters}>Importancia</Dropdown.Item>
-                        <Dropdown.Item name="categoria" onClick={handleFilters}>Categoria</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                    <OrderxFilter listaReportes={reportes} setReportesActivos={setReportesActivos} reportesActivos={reportesActivos} />
                     <div className="lista-reportes-container">
                         {
                             !reportesActivos.length ? <h3>Acá aparecerán tus reportes</h3> :
