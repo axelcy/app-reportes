c51c33cf9dedb4ec565be353cfb3348206ceaef4
Santiago Kasses <101513911+santiagokasses@users.noreply.github.com>
2023-09-01 10:36:30 -0300

    filtros terminados

--- src/Components/OrderxFilter.jsx
+++ src/Components/OrderxFilter.jsx
@@ -8,7 +8,8 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
 
     const [ascOrder, setAscOrder] = useState(false)
     const [inputType, setInputType] = useState("hidden")
-    const [inputType2, setInputType2] = useState("hidden")
+    const [inputTypeFecha1, setInputFecha1] = useState("hidden")
+    const [inputTypeFecha2, setInputFecha2] = useState("hidden")
     const [filterType, setFilterType] = useState('todo')
     const [filter, setFilter] = useState("")
     const [minDate, setMinDate] = useState('');
@@ -34,42 +35,48 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
 
     }
     const handleFilters = (e) => {
-        // if (e.target.name === 'inputFiltrosMin') {
-        //     setMinDate(e.target.value);
-        // } else if (e.target.name === 'inputFiltrosMax') {
-        //     setMaxDate(e.target.value);
-        // }
         if (e.target.name === 'inputFiltros') setFilter(e.target.value)
+        else if (e.target.name === 'inputFiltrosMin') {
+            setMinDate(e.target.value);
+        } 
+        else if (e.target.name === 'inputFiltrosMax') {
+            setMaxDate(e.target.value);
+        }
         else {
             document.getElementById("dropdown-basic2").innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"></path></svg> Ordenar por ${e.target.name}`
             if (e.target.name === 'todo') {
                 setReportesActivos(listaReportes)
                 setInputType('hidden')
-                setInputType2('hidden')
+                setInputFecha1('hidden')
+                setInputFecha2('hidden')
             }
             if (e.target.name === 'edificio') {
                 setReportesActivos(listaReportes)
                 setFilterType('edificio')
                 setInputType('number')
-                setInputType2('hidden')
+                setInputFecha1('hidden')
+                setInputFecha2('hidden')
             }
             if (e.target.name === 'fecha') {
                 setReportesActivos(listaReportes)
                 setFilterType('fecha')
-                setInputType('date')
-                setInputType2('date')
+                setInputType('hidden')
+                setInputFecha1('date')
+                setInputFecha2('date')
             }
             if (e.target.name === 'importancia') {
                 setReportesActivos(listaReportes)
                 setFilterType('importancia')
                 setInputType('number')
-                setInputType2('hidden')
+                setInputFecha1('hidden')
+                setInputFecha2('hidden')
             }
             if (e.target.name === 'categoria') {
                 setReportesActivos(listaReportes)
                 setFilterType('categoria')
                 setInputType('number')
-                setInputType2('hidden')
+                setInputFecha1('hidden')
+                setInputFecha2('hidden')
             }
         }
         console.log(reportesActivos)
@@ -77,15 +84,15 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
 
     useEffect(() => {
         if (filter === "") setReportesActivos(listaReportes)
-        else if (filterType === 'edificio') setReportesActivos(listaReportes.filter(({ idPisoAula }) => idPisoAula == filter))
-        // else if (filterType === 'fecha') {
-        //     if (minDate && maxDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) >= minDate && fecha.slice(0, 10) <= maxDate))
-        //     else if (minDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) >= minDate))
-        //     else if (maxDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10)  <= maxDate))
-        // }
-        else if (filterType === 'fecha') setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) == filter))
-        else if (filterType === 'importancia') setReportesActivos(listaReportes.filter(({ importancia }) => importancia == filter))
-        else if (filterType === 'categoria') setReportesActivos(listaReportes.filter(({ categoria }) => categoria == filter))
+        if (filterType === 'edificio') setReportesActivos(listaReportes.filter(({ idPisoAula }) => idPisoAula == filter))
+        if (filterType === 'fecha') {
+            if (minDate && maxDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) >= minDate && fecha.slice(0, 10) <= maxDate))
+            else if (minDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) >= minDate))
+            else if (maxDate) setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10)  <= maxDate))
+        }
+        // else if (filterType === 'fecha') setReportesActivos(listaReportes.filter(({ fecha }) => fecha.slice(0, 10) == filter))
+        if (filterType === 'importancia') setReportesActivos(listaReportes.filter(({ importancia }) => importancia == filter))
+        if (filterType === 'categoria') setReportesActivos(listaReportes.filter(({ categoria }) => categoria == filter))
         console.log(minDate, maxDate)
     }, [filter, minDate, maxDate])
 
@@ -107,8 +114,7 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
                 <div className="divOrder">
                     <Dropdown.Toggle variant="info" className="toggle" id="dropdown-basic2"><FaFilter /> Filtrar por </Dropdown.Toggle>
                     <input name="inputFiltros" onChange={handleFilters} type={inputType} />
-                    <input name="inputFiltros" onChange={handleFilters} type={inputType2} />
-                    {/* <input
+                    <input
                         name="inputFiltrosMin"
                         onChange={(e) => {
                             if (filterType === 'fecha') {
@@ -116,7 +122,7 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
                             }
                             handleFilters(e);
                         }}
-                        type={inputType}
+                        type={inputTypeFecha1}
                         value={minDate}
                     />
                     <input
@@ -127,9 +133,9 @@ function OrderxFilter({ listaReportes, setReportesActivos, reportesActivos }) {
                             }
                             handleFilters(e);
                         }}
-                        type={inputType2}
+                        type={inputTypeFecha2}
                         value={maxDate}
-                    /> */}
+                    />
                 </div>
                 <Dropdown.Menu className="dropdown">
                     <Dropdown.Item className="Dropdown-item" name="todo" onClick={handleFilters}>Todo</Dropdown.Item>
